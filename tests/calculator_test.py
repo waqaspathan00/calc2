78c6d21e3fd95498f33ca9032650cf0bc8858fbf
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-10-23 11:09:29 -0400

    added tests for newly created calc method

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,23 +6,35 @@ def test_calculator_result():
     calc = Calculator()
     assert calc.result == 0
 
-def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(1)
-    #Assert that the results are correct
-    assert calc.result == 1
-
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
+
+    assert calc.get_result() == 0
+
+def test_calculator_add():
+    """Testing the Add function of the calculator"""
+    # ARRANGE by instantiating the calc class
+    calc = Calculator()
+    # ACT by calling the method to be tested
     calc.add_number(1)
-    assert calc.get_result() == 1
+    # ASSERT that the results are correct
+    assert calc.result == 1
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+
+def test_calculator_multiply():
+    """Testing the multiply method of the calculator"""
+    calc = Calculator()
+    calc.multiply_number(1)
+    assert calc.get_result() == 0
+
+def test_calculator_divide():
+    """Testing the divide method of the calculator"""
+    calc = Calculator()
+    calc.divide_number(1)
+    assert calc.get_result() == 0
