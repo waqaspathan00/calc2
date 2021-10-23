54a2279b54e5bcc3ee246762b1dba0d111d09ca6
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-10-23 11:13:43 -0400

    added spacing

--- tests/multiple_vals_test.py
+++ tests/multiple_vals_test.py
@@ -2,18 +2,18 @@
 from calculator.main import Calculator
 
 def test_calculator_result():
-    """testing calculator result is 0"""
+    """ testing calculator result is 0 """
     calc = Calculator()
     assert calc.result == 0
 
 def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
+    """ Testing the Get result method of the calculator """
     calc = Calculator()
 
     assert calc.get_result() == 0
 
 def test_calculator_add_multiple():
-    """Testing the add function of the calculator with multiple vals"""
+    """ Testing the add function of the calculator with multiple vals """
     # ARRANGE by instantiating the calc class
     calc = Calculator()
     # ACT by calling the method to be tested
@@ -22,20 +22,20 @@ def test_calculator_add_multiple():
     assert calc.result == 6
 
 def test_calculator_subtract_multiple():
-    """Testing the subtract method of the calculator with multiple vals"""
+    """ Testing the subtract method of the calculator with multiple vals """
     calc = Calculator()
     calc.subtract_number(5, 10)
     assert calc.get_result() == -15
 
 def test_calculator_multiply_multiple():
-    """Testing the multiply method of the calculator with multiple vals"""
+    """ Testing the multiply method of the calculator with multiple vals """
     calc = Calculator()
     calc.add_number(1)
     calc.multiply_number(5, 4, 5)
     assert calc.get_result() == 100
 
 def test_calculator_divide_multiple():
-    """Testing the divide method of the calculator with multiple vals"""
+    """ Testing the divide method of the calculator with multiple vals """
     calc = Calculator()
     calc.add_number(12)
     calc.divide_number(3, 2)
