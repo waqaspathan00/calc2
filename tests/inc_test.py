daec660ee04c65db544e727059bf74692f8f6018
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-10-22 23:54:55 -0400

    removed duplicate code

--- tests/inc_test.py
+++ tests/inc_test.py
@@ -4,6 +4,3 @@ from calculator.main import inc
 def test_answer():
     """This Tests the function"""
     assert inc(4) == 5
-def test_spam():
-    """This Tests the function"""
-    assert inc(4) == 5
