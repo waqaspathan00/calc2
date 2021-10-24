3210f7589aa7ada41c898acc02b84bdcbd5fa84a
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-10-24 18:09:29 -0400

    spacing

--- readme.md
+++ readme.md
@@ -6,5 +6,7 @@ To run tests, Lint, and Coverage report use this command:
 pytest  --pylint --cov
 
 .pylintrc is the config for pylint
+
 .coveragerc is the config for coverage
+
 setup.py is a config file for pytest
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,19 +1,20 @@
 """Testing the Calculator"""
+import pytest
 from calculator.main import Calculator
 
 def test_calculator_result():
-    """testing calculator result is 0"""
+    """ Testing calculator result is 0 """
     calc = Calculator()
     assert calc.result == 0
 
 def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
+    """ Testing the Get result method of the calculator """
     calc = Calculator()
 
     assert calc.get_result() == 0
 
 def test_calculator_add():
-    """Testing the Add function of the calculator"""
+    """ Testing the Add function of the calculator """
     # ARRANGE by instantiating the calc class
     calc = Calculator()
     # ACT by calling the method to be tested
@@ -22,19 +23,19 @@ def test_calculator_add():
     assert calc.result == 1
 
 def test_calculator_subtract():
-    """Testing the subtract method of the calculator"""
+    """ Testing the subtract method of the calculator """
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
 
 def test_calculator_multiply():
-    """Testing the multiply method of the calculator"""
+    """ Testing the multiply method of the calculator """
     calc = Calculator()
     calc.multiply_number(1)
     assert calc.get_result() == 0
 
 def test_calculator_divide():
-    """Testing the divide method of the calculator"""
+    """ Testing the divide method of the calculator """
     calc = Calculator()
     calc.divide_number(1)
     assert calc.get_result() == 0
