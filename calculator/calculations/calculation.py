715e361d1d60613489d70a7df1654054855fb28e
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-11-09 11:10:12 -0500

    rewrote module and class docstring to be more descriptive

--- calculator/calculations/calculation.py
+++ calculator/calculations/calculation.py
@@ -1,7 +1,10 @@
-""" this is an interface class for math operations to be built on top of """
+""" this module contains the calculation class """
 
 class Calculation:  # pylint: disable=too-few-public-methods
-    """when instantiated, it will take the numbers in a math calculation as parameters"""
+    """
+    this is an interface class for math operations to be built on top of
+    when instantiated, it will take the numbers in a math calculation as parameters
+    """
 
     def __init__(self, *vals):
         self.vals = vals
