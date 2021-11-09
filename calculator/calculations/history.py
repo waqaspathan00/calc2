5b7846fe12fab77ca71e29393bcc32289beedc78
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-11-09 11:01:00 -0500

    refactored use of Calculator to new History object

--- calculator/calculations/history.py
+++ calculator/calculations/history.py
@@ -4,24 +4,24 @@ class History:
     @staticmethod
     def get_first_calculation():
         """ return the first calculation in Calculator history """
-        return Calculator.history[0]
+        return History.history[0]
 
     @staticmethod
     def get_last_calculation():
         """ return the last calculation in Calculator history """
-        return Calculator.history[-1]
+        return History.history[-1]
 
     @staticmethod
     def get_num_of_calculations():
         """ return the number of calculations in Calculator history """
-        return len(Calculator.history)
+        return len(History.history)
 
     @staticmethod
     def clear_history():
         """ remove all calculations in Calculator history """
-        Calculator.history = []
+        History.history = []
 
     @staticmethod
     def remove_from_history(index):
         """ remove a calculation at specified index in Calculator history """
-        Calculator.history.pop(index)
+        History.history.pop(index)
