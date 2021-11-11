a3e903d1c9b09432aa39f008625475395aa6094d
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-11-11 01:13:57 -0500

    simplified Calculator logic in main

--- calculator/main.py
+++ calculator/main.py
@@ -1,50 +1,35 @@
 """ This is the Calculator object which can do basic calculator functions: +-*/ """
-from calculator.calculations.operations import Addition, Subtraction, Multiplication, Division
 from calculator.calculations.history import History
 
 class Calculator:
     """ This is the Calculator class"""
 
-    # IGNORE - CURRENTLY DEPRECATED
-    # def __init__(self):
-    #     self.result = 0
-    #
-    # def get_result(self):
-    #     """ Get Result of Calculation"""
-    #     return self.result
-
     @staticmethod
     def add_number(*vals):
-        """ adds values to result """
-        total = Addition(*vals)
-
-        History.history.append(total)
-
-        return total.get_result()
+        """ performs addition and returns resulting value """
+        History.add_addition_calculation(*vals)
+        return Calculator.get_last_result()
 
     @staticmethod
     def subtract_number(*vals):
-        """ subtract val from result """
-        total = Subtraction(*vals)
-
-        History.history.append(total)
-
-        return total.get_result()
+        """ performs subtraction and returns resulting value """
+        History.add_subtraction_calculation(*vals)
+        return Calculator.get_last_result()
 
     @staticmethod
     def multiply_number(*vals):
-        """ multiply result by number """
-        total = Multiplication(*vals)
-
-        History.history.append(total)
-
-        return total.get_result()
+        """ performs addition and returns resulting value """
+        History.add_multiplication_calculation(*vals)
+        return Calculator.get_last_result()
 
     @staticmethod
     def divide_number(*vals):
-        """ divide result by number """
-        total = Division(*vals)
+        """ performs addition and returns resulting value """
+        History.add_division_calculation(*vals)
+        return Calculator.get_last_result()
 
-        History.history.append(total)
-
-        return total.get_result()
+    @staticmethod
+    def get_last_result():
+        """ return the result of the last calculation """
+        calculation = History.get_last_calculation()
+        return calculation.get_result()
