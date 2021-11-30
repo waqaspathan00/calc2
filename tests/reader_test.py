9e958ac4cb7cf4198197fd3c27d4dc3b9edeeffc
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-11-30 10:14:09 -0500

    wrote tests for large data files, logically not different from small data files

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -1,18 +1,22 @@
-from calculator.helpers.csv_reader import Reader
+import pytest
+from calculator.helpers.csv_handler import Reader
 from calculator.main import Calculator
 
+
 def test_get_operation_name():
+    """  """
     # ARRANGE
-    reader = Reader("tests/test_data/addition.csv")
+    reader = Reader("tests/input_data/addition.csv")
 
     # ACT
-    operation_name = reader.get_operation_name()
+    operation_name = reader._get_operation_name()
 
     # ASSERT
     assert operation_name == "addition"
 
 def test_addition_using_file():
-    reader = Reader("tests/test_data/addition.csv")
+    """  """
+    reader = Reader("tests/input_data/addition.csv")
     operation = reader.operation
 
     for row in reader.rows:
@@ -21,10 +25,10 @@ def test_addition_using_file():
         calculated_result = Calculator.calculate_numbers(operation, *vals)
 
         assert expected_result == calculated_result
-
 
 def test_subtraction_using_file():
-    reader = Reader("tests/test_data/subtraction.csv")
+    """  """
+    reader = Reader("tests/input_data/subtraction.csv")
     operation = reader.operation
 
     for row in reader.rows:
@@ -33,10 +37,10 @@ def test_subtraction_using_file():
         calculated_result = Calculator.calculate_numbers(operation, *vals)
 
         assert expected_result == calculated_result
-
 
 def test_multiplication_using_file():
-    reader = Reader("tests/test_data/multiplication.csv")
+    """  """
+    reader = Reader("tests/input_data/multiplication.csv")
     operation = reader.operation
 
     for row in reader.rows:
@@ -45,10 +49,26 @@ def test_multiplication_using_file():
         calculated_result = Calculator.calculate_numbers(operation, *vals)
 
         assert expected_result == calculated_result
-
 
 def test_division_using_file():
-    reader = Reader("tests/test_data/division.csv")
+    """  """
+    reader = Reader("tests/input_data/division.csv")
+    operation = reader.operation
+
+    # why does this work?
+    # what if i run a test file that doesnt divide by 0
+
+    with pytest.raises(ZeroDivisionError):
+        for row in reader.rows:
+            expected_result = row[0]
+            vals = row[1:]
+            calculated_result = Calculator.calculate_numbers(operation, *vals)
+
+        assert expected_result == calculated_result
+
+def test_large_addition_using_file():
+    """  """
+    reader = Reader("tests/input_data/large_addition.csv")
     operation = reader.operation
 
     for row in reader.rows:
@@ -58,6 +78,29 @@ def test_division_using_file():
 
         assert expected_result == calculated_result
 
+def test_large_subtraction_using_file():
+    """  """
+    reader = Reader("tests/input_data/large_subtraction.csv")
+    operation = reader.operation
+
+    for row in reader.rows:
+        expected_result = row[0]
+        vals = row[1:]
+        calculated_result = Calculator.calculate_numbers(operation, *vals)
+
+        assert expected_result == calculated_result
+
+def test_large_multiplication_using_file():
+    """  """
+    reader = Reader("tests/input_data/large_multiplication.csv")
+    operation = reader.operation
+
+    for row in reader.rows:
+        expected_result = row[0]
+        vals = row[1:]
+        calculated_result = Calculator.calculate_numbers(operation, *vals)
+
+        assert expected_result == calculated_result
 
 """
 make a fixture to load the csv file and perform calculations
