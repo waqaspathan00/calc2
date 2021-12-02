7a8bbb4d4328f7bcaf4554b685d67bcc609dc1f4
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-12-01 23:45:36 -0500

    formatting and cleanup

--- calculator/main.py
+++ calculator/main.py
@@ -1,6 +1,5 @@
 """ This is the Calculator object which can do basic calculator functions: +-*/ """
 from calculator.helpers.history import History
-from calculator.helpers.csv_handler import Reader
 
 class Calculator:
     """ This is the Calculator class"""
