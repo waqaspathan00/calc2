eb336e3181b94d4fab849f88e634b73a43f68f27
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-12-05 00:49:08 -0500

    cleaned up and refactored code to be my own

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -9,18 +9,19 @@ class CalculatorController(ControllerBase):
         if request.form['value1'] == '' or request.form['value2'] == '':
             error = 'You must enter a value for value 1 and or value 2'
         else:
-            flash('You successfully calculated')
-            flash('You are awesome')
+            flash('Successfully calculated')
 
             # get the values out of the form
             value1 = request.form['value1']
             value2 = request.form['value2']
             operation = request.form['operation']
+
             # this will call the correct operation
             Calculator.calculate_numbers(operation, value1, value2)
             result = str(Calculator.get_last_result())
-            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result, nums=5)
         return render_template('calculator2.html', error=error)
+
     @staticmethod
     def get():
         return render_template('calculator2.html')
