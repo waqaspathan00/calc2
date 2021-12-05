026b66c4f8ff16c2cfa0662bc856a42892fc914e
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-12-05 02:07:36 -0500

    calculator has complete core functionality. will allow input for numbers and operation then compute properly

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -1,14 +1,22 @@
 from app.controllers.controller import ControllerBase
 from flask import render_template, request
+from calculator.main import Calculator
 
 class IndexController(ControllerBase):
 
     @staticmethod
     def post():
+        """
+        get the numbers entered from the form
+        then perform calculation specified by user
+        """
         nums = [int(num) for num in request.form["nums"].split()]
+        operation = request.form['operation']
 
+        Calculator.calculate_numbers(operation, *nums)
+        result = str(Calculator.get_last_result())
 
-        return render_template("index.html")
+        return render_template('result.html', operation=operation, nums=nums, result=result)
 
     @staticmethod
     def get():
