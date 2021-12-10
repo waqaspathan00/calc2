c72f9756cd3fa4a9434cbbcdcaf4357bd34f35e5
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-12-10 18:44:29 -0500

    began logic for showing calculation history. as of right now shows static history.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 """A simple flask web app"""
 from flask import Flask
 from app.controllers.index_controller import IndexController
-from app.controllers.calculator_controller import CalculatorController
+from app.controllers.history_controller import HistoryController
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -13,6 +13,10 @@ def index_get():
 @app.route("/", methods=['POST'])
 def index_post():
     return IndexController.post()
+
+@app.route("/history")
+def history():
+    return HistoryController.get()
 
 # @app.route("/calculator", methods=['GET'])
 # def calculator_get():
